11abb9fa586fd50a7385f8cc2c214ae01ea17a02
mtgorski <mgorski8@gmail.com>
2016-01-13 18:46:19 -0500

    Adds MVC project for demo

---
+++ AsyncProfilingDemo.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AsyncProfilingDemo.MVC.MvcApplication" Language="C#" %>
---
+++ AsyncProfilingDemo.MVC/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using ServiceStack.MiniProfiler.EF6;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AsyncProfilingDemo.MVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Setup profiler for Controllers via a Global ActionFilter
+            GlobalFilters.Filters.Add(new ProfilingActionFilter());
+
+            // initialize automatic view profiling
+            var copy = ViewEngines.Engines.ToList();
+            ViewEngines.Engines.Clear();
+            foreach (var item in copy)
+            {
+                ViewEngines.Engines.Add(new ProfilingViewEngine(item));
+            }
+
+
+            MiniProfilerEF6.Initialize();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
+        }
+    }    
+}
